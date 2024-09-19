083f6091f02384d610e5df4d35ab588280d2b744
Jannah Kurniawati <jannahkurnia15@gmail.com>
2024-09-19 20:17:48 +0700

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Jannah Kurniawati");
+        System.out.println("NIM: 607062400024");
+        System.err.println("Kelas: D3IF-48-01");
+    }
+}
